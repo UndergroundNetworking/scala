2acd6c081b43d9aca98bc95bd9ab47385a7b8a5b
mharlos <mharlos@janrain.com>
2013-04-23 12:35:18 -0700

    Added the printing of magnet links

--- bay.scala
+++ bay.scala
@@ -2,10 +2,17 @@ import getRequest.getUrl
 
 object bay{
 	def getBay() :String = {
-		val content = getRequest.getUrl("http://thepiratebay.se/search/bones/0/7/0")
+		val content = getRequest.getUrl("http://thepiratebay.se")
 		return content
 	}
-	def searchBay(method:String,search:String):String = {
+	def searchBay(query:String,method:String):Array[String] = {
+		val sUrl = "http://thepiratebay.se/search/" + query + method
+		println(sUrl)
+		val content = getRequest.getUrl(sUrl)
+		val sContent = content.split("\n")
+		return sContent
+	}
+	def setSearch(method:String):String = {
 		var dmethod = "/0/7/0"
 		if (method == "seed"){
 			dmethod = "/0/7/0"
@@ -15,6 +22,32 @@ object bay{
 		}
 		return dmethod
 	}
+	def printRawLines(html:String):Array[String] = {
+		val lines = html.split("\n")
+		val lineCount = lines.length
+		for (x <- 1 to(lineCount - 1)){
+			println(lines.deep(x))
+		}
+		return lines
+	}
+	def getMagnets(x:Array[String]){
+		for (a <- 1 to (x.length - 1)){
+			val mLines = x.deep(a).toString
+			if (mLines contains "<a href=\"magnet"){
+				println("")
+				println(a)
+				val title = mLines.split("=")
+				val nTitle = title.deep(3).toString
+				val eTitle = nTitle.replace("+"," ")
+				println(eTitle)
+				println("")
+				val mLink = mLines.split("\"")
+				println(mLink.deep(1))
+			}
+
+		}
+	}
+
 	def main(args:Array[String]){
 		val site = getBay()
 		println(site)
